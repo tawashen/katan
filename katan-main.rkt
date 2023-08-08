f7ebb05a149c11ceafe7fa9c09166878a1e6676e
tawashen <tawashen@gmail.com>
2023-08-08 22:24:33 +0900

    ロード

--- katan-main.rkt
+++ katan-main.rkt
@@ -11,32 +11,51 @@
 (struct CARD (WOOD IRON SHEEP BLOCK))
 
 (define *map-zero* '(
-                     (0 1 2 3 4)
-                     (1 2 3 4 0)
-                     (2 3 4 0 1)
-                     (3 4 0 1 2)
-                     (4 0 1 2 3)))
+                     (1 2 3 4)
+                     (2 3 4 1)
+                     (3 4 1 2)
+                     (4 1 2 3)
+                     ))
 
-(define (x32 x y)
-  (make-posn (case x
-                ((1) 32)
-                (else (+ (* x 32) (* (- x 1) 32))))
-              (case y
-                ((1) 32)
-                 (else (+ (* y 32) (* (- y 1) 32))))))
+(define (x40 x y)
+  (make-posn (+ (* x 40) (* (- x 1) 40))
+             (+ (* y 40) (* (- y 1) 40))))
 
-(place-images
- (map (lambda (x) (square 64 "solid"
+
+(define (place-map)
+(place-images/align
+ (map (lambda (x) (square 80 "solid"
                          (case x
-                           ((0) "red")
-                           ((1) "white")
-                           ((2) "green")
-                           ((3) "yellow")
+                           ((1) "red")
+                           ((2) "white")
+                           ((3) "green")
+                           ((4) "yellow")
                            (else "blue")))) (flatten *map-zero*))
-  (flatten (for/list ((i (iota 5 1 1)))
-             (for/list ((j (iota 5 1 1)))
-    (x32 i j))))
- (rectangle 320 320 "solid" "blue"))
+  (flatten (for/list ((i (iota 4 1 1)))
+             (for/list ((j (iota 4 1 1)))
+    (x40 i j)))) "left" "top"
+ (rectangle 400 400 "solid" "blue")))
+
+(define (place-town)
+(place-images/align
+ (list (text "●" 30 "olive"))
+ (list (make-posn 32 28))
+ "left" "top" (place-map)))
+
+
+(define (place-road)
+  (place-images/align
+   (list (rectangle 60 10 "solid" "black")
+         (circle 10 "solid" "yellow"))
+   (list (make-posn 50 36)
+         (make-posn 30 30))
+   "left" "top" (place-town)))
+
+(place-road)
+
+
+
+
 
 
 
