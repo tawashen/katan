2b1b516dca4aac55622610a6fe8e9d62258b7506
tawashen <tawashen@gmail.com>
2023-08-10 22:40:55 +0900

    map ok

--- katan-main.rkt
+++ katan-main.rkt
@@ -17,6 +17,11 @@
                      (4 1 2 3)
                      ))
 
+
+(define *cross-p* '(#f #f #f #f #f #f #f #f #f 1 #f #f #f #f #f #f 2 #f #f #f #f #f #f #f #f)) ;25
+(define *roads-p* '(1 1 #f #f #f #f #f #f 1 #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f 2
+                     #f #f #f #f #f 2 #f #f #f #f #f #f #f #f 1)) ;40
+
 (define (x40 x y)
   (make-posn (+ (* x 40) (* (- x 1) 40))
              (+ (* y 40) (* (- y 1) 40))))
@@ -38,25 +43,92 @@
 
 (define (place-town)
 (place-images/align
- (list (text "●" 30 "olive"))
- (list (make-posn 32 28))
+ (list
+  ;(text "●" 30 "olive")
+  )
+ (list
+  ;(make-posn 32 28)
+  )
  "left" "top" (place-map)))
 
 
+(define (tate&yoko x)
+  (cond ((<= x 4) 'yoko)
+        ((and (>= x 5) (<= x 9)) 'tate)
+        ((and (>= x 10) (<= x 13)) 'yoko)
+        ((and (>= x 14) (<= x 18)) 'tate)
+        ((and (>= x 19) (<= x 22)) 'yoko)
+        ((and (>= x 23) (<= x 27)) 'tate)
+        ((and (>= x 28) (<= x 31)) 'yoko)
+        ((and (>= x 32) (<= x 36)) 'tate)
+        (else 'yoko)))
+
+(define (tate&yokoX x)
+  (cond ((<= x 4) (+ 50 (* 80 (- x 1))))
+        ((and (>= x 5) (<= x 9)) (+ 34 (* 80 (- x 5))))
+        ((and (>= x 10) (<= x 13)) (+ 50 (* 80 (- x 10))))
+        ((and (>= x 14) (<= x 18)) (+ 34 (* 80 (- x 14))))
+        ((and (>= x 19) (<= x 22)) (+ 50 (* 80 (- x 19))))
+        ((and (>= x 23) (<= x 27)) (+ 34 (* 80 (- x 23))))
+        ((and (>= x 28) (<= x 31)) (+ 50 (* 80 (- x 28))))
+        ((and (>= x 32) (<= x 36)) (+ 34 (* 80 (- x 32))))
+        (else (+ 50 (* 80 (- x 37))))))
+
+(define (tate&yokoY x)
+    (cond ((<= x 4) 36)
+        ((and (>= x 5) (<= x 9)) 50)
+        ((and (>= x 10) (<= x 13)) 116)
+        ((and (>= x 14) (<= x 18)) 130)
+        ((and (>= x 19) (<= x 22)) 196)
+        ((and (>= x 23) (<= x 27)) 210)
+        ((and (>= x 28) (<= x 31)) 276)
+        ((and (>= x 32) (<= x 36)) 290)
+        (else 356)))
+  
+
 (define (place-road)
   (place-images/align
-   (list (rectangle 60 10 "solid" "black")
-         (circle 10 "solid" "yellow"))
-   (list (make-posn 50 36)
-         (make-posn 30 30))
+ ;  (list
+   ; (circle 10 "solid" "yellow")
+    (let loop ((lst *roads-p*) (count 1) (acc '()))
+      (if (null? lst)
+          (reverse acc)
+          (loop (cdr lst) (+ count 1) (if (car lst) (cons (case (tate&yoko count)
+                                                      ((yoko) (rectangle 60 10 "solid" (case (car lst)
+                                                                                         ((1) "black")
+                                                                                         ((2) "blue"))))
+                                                      ((tate) (rectangle 10 60 "solid" (case (car lst)
+                                                                                         ((1) "black")
+                                                                                         ((2) "blue"))))) acc)
+                                          acc))))
+    (let loopB ((lst *roads-p*) (count 1) (acc '()))
+      (if (null? lst)
+          (reverse acc)
+          (loopB (cdr lst) (+ count 1) (if (car lst) (cons (make-posn (tate&yokoX count) (tate&yokoY count)) acc) acc))))
+                                                             
+  ;                     (
+   ; (rectangle 60 10 "solid" "black")
+   ; (rectangle 60 10 "solid" "black")
+   ; (rectangle 60 10 "solid" "black")
+
+   ; (rectangle 10 60 "solid" "white")
+   ; (rectangle 10 60 "solid" "blue")
+   ; (rectangle 10 60 "solid" "blue")
+     ;    )
+ ;  (list
+ ;   (make-posn 30 30)
+  ;  (make-posn 50 36)
+  ;  (make-posn 130 36)
+  ;  (make-posn 50 116)
+    
+   ; (make-posn 34 50)　　
+   ; (make-posn 114 50)
+   ; (make-posn 114 130)
+    ;     )
    "left" "top" (place-town)))
 
 (place-road)
 
-(define cross-p '(#f #f #f #f #f #f #f #f #f 1 #f #f #f #f #f #f 2 #f #f #f #f #f #f #f #f)) ;25
-
-(define roads-p '(#f #f #f #f #f #f #f #f 1 #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f 2
-                     #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)) ;40
 
 
 
