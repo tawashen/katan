54bc967150d4beb5472a318037ee32338aeb2c81
tawashen <tawashen@gmail.com>
2023-08-21 00:11:23 +0900

    nayamityu

--- test.rkt
+++ test.rkt
@@ -8,18 +8,18 @@
                      #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)) ;
 
 
-(define left (lambda (road-map c-point gyou-num)
-               (with-handlers ((exn:fail? (const #f))) (and (list-ref road-map (+ (- (- c-point 1) 1) gyou-num));
-                                                        (not (= 0 (remainder (- c-point 1) 5))))))) ;
-(define up (lambda (road-map c-point gyou-num)
-              (with-handlers ((exn:fail? (const #f))) (and (list-ref road-map (+ (- c-point 1) gyou-num))
-                                                        (not (<= c-point 4))))))
-(define right (lambda (road-map c-point gyou-num)
-                 (with-handlers ((exn:fail? (const #f))) (and (list-ref road-map (+ (- c-point 1) gyou-num))
-                                                        (not (= 0 (remainder c-point 5)))))))
-(define down (lambda (road-map c-point gyou-num)
-               (with-handlers ((exn:fail? (const #f))) (and (list-ref road-map (+  (+ c-point 3) gyou-num))
-                                                        (not (>= c-point 21))))))
+(define left (lambda (road-map c-point)
+               (with-handlers ((exn:fail? (const #f))) (and (list-ref road-map (- (+ (- c-point 1) (* 4 (quotient (- c-point 1) 5))) 1));
+                                                        (not (member c-point '(1 6 11 16 20))))))) ;
+(define up (lambda (road-map c-point)
+              (with-handlers ((exn:fail? (const #f))) (and (list-ref road-map (- (+ (- c-point 5) (* 4 (quotient (- c-point 1) 5))) 1))
+                                                        (not (member  c-point '(1 2 3 4 5)))))))
+(define right (lambda (road-map c-point)
+                 (with-handlers ((exn:fail? (const #f))) (and (list-ref road-map  (- (+ c-point (* 4 (quotient (- c-point 1) 5))) 1))
+                                                        (not (member c-point '(5 10 15 20 25)))))))
+(define down (lambda (road-map c-point)
+               (with-handlers ((exn:fail? (const #f))) (and (list-ref road-map (- (+ (+ c-point 4) (* 4 (quotient (- c-point 1) 5))) 1))
+                                                        (not (member c-point '(21 22 23 24 25)))))))
 (define check-funcs `(,left ,up ,right ,down))
                
 
@@ -28,10 +28,30 @@
     (let loop ((funcs check-funcs) (counter 0))
       (if (null? funcs) (cond ((= 1 counter) #t)
                                           (else #f))
-          (loop (cdr funcs) (if ((car funcs) roads c-point gyou-num)
+          (loop (cdr funcs) (if ((car funcs) roads c-point)
                                 (+ counter 1) counter))))))
 
-(hazi? *roads-p* 5)
+; (filter (lambda (x) (hazi? *roads-p* x)) (iota 25 1 1))
+
+
+(define (dokohe? roads c-point)
+  (cond ((left roads c-point) (- c-point 1))
+        ((up roads c-point) (- c-point 5))
+        ((right roads c-point) (+ c-point 1))
+        ((down roads c-point) (+ c-point 5))))
+
+
+(dokohe? *roads-p* 1)
+
+(define (tadoru roads c-point point-list bunki max)
+  (if (and (null? bunki) (car (dokohe? roads c-point)
+
+(let loop ((roads *roads-p*) (c-point 1) (point-list '(1)) (bunki '()) (max 0))
+  (if (and (null? bunki) (dokohe? roads 
 
 
 (define (max-length roads cross-p)
+  (let ((hazi-points (filter (lambda (x) (hazi? roads x)) (iota 25 1 1))))
+  (let loop ((roads roads) (hazi-points hazi-points) (points '()) (max #f))
+    (if (null? hazi-points) length
+        (
