99f6a4c5dd34f2657d2932344b7f40ebe681b9d1
tawashen <tawashen@gmail.com>
2023-08-25 00:01:03 +0900

    deketa

--- test.rkt
+++ test.rkt
@@ -5,7 +5,7 @@
 
 
 (define *roads-p1* '(1 #f #f 1 #f 1 #f #f 1 1 #f #f #f 1 #f 1 1 1 1 #f 1 #f #f 1 #f
-                    #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)) ;
+                     #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)) ;
 
 (define *roads-p2* '(1 #f #f 1 #f 1 #f #f 1 1 1 #f #f 1 #f 1 1 1 1 #f 1 #f #f 1 1
                      #f #f #f #f #f #f #f #f 1 #f #f #f #f #f #f)) 
@@ -45,27 +45,25 @@
     (if (func roads c-point) change '()))))
 
 
-
 (define (ippon-length roads c-point)
   (let loop ((c-point-d c-point) (point-list `(,c-point)) (pre-p #f))
-    (cond ((and (not (= c-point (car point-list))) (hazi? roads c-point-d)) (display (reverse point-list)));終了
+    (cond ((and (not (= c-point (car point-list))) (hazi? roads c-point-d)) (reverse point-list));(display (reverse point-list)));終了
         ;  ((member c-point-d point-list) (display (reverse point-list)));円環パターン終了
           (else
            (let ((num (remove pre-p (dokohe? roads c-point-d))))
-             (cond ((>= (length num) 2)
+           ;  (display num)
+          ;   (cond ((>= (length num) 2)
                     (for/list ((num num))
-                      (loop num (cons num point-list) c-point-d)))
-                          ;  (if (< (length max) (length point-list))
-                          ;  point-list)))
+                      (loop num (cons num point-list) c-point-d)))))))
+                           ; (if (< (length max) (length point-list))
+                           ; point-list)))))))
                             ;max))))
-                   (else
-                   (loop (car num) (cons (car num)  point-list) c-point-d))))))))
-                          ;(if (< (length max) (length point-list))
-                        ; point-list))))))))
-                         ;max)))))))))
+               ;    (else
+                  ;  (loop (car num) (cons (car num)  point-list) c-point-d
+                         ; (if (< (length max) (length point-list)) point-list max)))))))))
 
 
-(ippon-length *roads-p1* 1)
+(ippon-length *roads-p2* 1)
 
 ;(define (
 
@@ -87,3 +85,56 @@
     (if (null? hazi-points) length
         (
 |#
+
+
+(define test '((    (       ((((1 2 7 6 11 12 17))))       ((((1 2 7 8 13 14 9)) ((1 2 7 8 13 18 23))))  ) ) ) )
+
+
+(define (tansaku lst acc)
+  (cond ((number? (car lst)) acc) ;数字まで到達したら終了
+        ((and (list? (car lst)) (number? (car (car lst)))) (tansaku (car lst) (cons (car lst) acc)));()に数字の入ってるリストが来たらAccに追加
+        ((not (null? (cdr lst))) (tansaku (car lst) acc)  (tansaku (cdr lst) acc));Cdr部がNullでなければCar、Cdrをそれぞれ再帰
+        ((and (list? (car lst)) (not (number? (car (car lst))))) (tansaku (car lst) acc))));Carで一段階中に入ってそのリストのCarが数字でなければCarで再帰
+
+;(tansaku test '())
+
+
+
+
+
+
+(define tes1 '((1 2 7 6 11 12 17)))
+(define tes2 '(((1 2 7 6 11 12 17))))
+#|
+(and (list? (car tes1)) (number? (car (car tes1))))
+(and (list? (car tes2)) (not (number? (car (car tes2)))))
+(cdr tes1)
+(cdr tes2)
+|#
+
+(define test2 '(       ((((1 2 7 6 11 12 17))))       ((((1 2 7 8 13 14 9)) ((1 2 7 8 13 18 23))))))
+;(cdr test2)
+
+(define (search-for-numbers lst)
+  (cond
+    [(null? lst) '()] ; リストが空の場合は空リストを返す
+    [(number? (car lst)) ; 先頭が数値の場合
+     (cons (car lst) (search-for-numbers (cdr lst)))] ; 数値を結果のリストに追加し、残りの部分に再帰
+    [(list? (car lst)) ; 先頭がリストの場合
+     (append (search-for-numbers (car lst)) (search-for-numbers (cdr lst)))] ; リスト内を再帰的に検索
+    [else
+     (search-for-numbers (cdr lst))])) ; それ以外の場合はスキップして次の要素を探索
+
+(define testv '((    (       ((((1 2 7 6 11 12 17))))       ((((1 2 7 8 13 14 9)) ((1 2 7 8 13 18 23))))  ) ) ) )
+
+;(search-for-numbers testv)
+
+(define test3 '(1 2 7 6 11 12 17 1 2 7 8 13 14 9 1 2 7 8 13 18 23))
+
+(define (devide-numbers lst c-point)
+  (let loop ((lst lst) (acc '()) (temp '()))
+  (cond ((null? lst) (reverse (cons (reverse temp) acc)))
+        ((= (car lst) c-point) (loop (cdr lst) (if (null? temp) acc (cons (reverse temp) acc)) (list c-point)))
+        (else (loop (cdr lst) acc (cons (car lst) temp))))))
+
+(devide-numbers test3 1)
