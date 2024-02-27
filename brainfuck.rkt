1d79ccd24fea112f7061cdccf3489fe10753c63c
tawashen <tawashen@gmail.com>
2024-02-27 21:37:56 +0900

    brain-fuck2

--- brainfuck.rkt
+++ brainfuck.rkt
@@ -10,21 +10,58 @@
   (let ((num (vector-length vec)))
     (vector-ref vec (- num 1))))
 
+(define @> (lambda (vec)
+            (vector-set! vec (- (vector-length vec) 1) (+ (vector-last vec) 1))
+            (brain-eval vec)))
+(define @< (lambda (vec)
+            (vector-set! vec (- (vector-length vec) 1) (- (vector-last vec) 1))
+            (brain-eval vec)))
+(define @+ (lambda (vec)
+            (vector-set! vec (vector-last vec) (+ (vector-ref vec (vector-last vec)) 1))
+            (brain-eval vec)))
+(define @- (lambda (vec)
+            (vector-set! vec (vector-last vec) (- (vector-ref vec (vector-last vec)) 1))
+            (brain-eval vec)))
+(define |@.| (lambda (vec)
+              (begin (displayln (integer->char (vector-ref vec (vector-last vec)))) (brain-eval vec))))
+(define |@,| (lambda (vec)
+              (displayln (integer->char (vector-ref vec (vector-last vec))))
+              (let ((new-v (string-ref (read-line) 0)))
+                (vector-set! vec (vector-last vec) (char->integer new-v)) (brain-eval vec))))
+(define |[| (lambda (vec)
+              (let loop ((answer (read-line)) (acc '()))
+                (case answer
+                  ((">") (loop (read-line) (cons @> acc)))
+                  (("<") (loop (read-line) (cons @< acc)))
+                  (("+") (loop (read-line) (cons @+ acc)))
+                  (("-") (loop (read-line) (cons @- acc)))
+                  ((".") (loop (read-line) (cons |@.| acc)))
+                  ((",") (loop (read-line) (cons |@,| acc)))
+                  (("]") (let loop ((commands (reverse acc)) (new-vec vec))
+                           (if (zero? (vector-ref new-vec (vector-last new-vec))) new-vec
+                               (loop (cdr commands) ((car commands) new-vec)))))
+                  (else (loop (read-line) acc))))))
+                  
+                  
+                
+              
+
 (define (brain-eval world)
   (let ((point (vector-last world)))
     (display point)
     (let ((command (read-line)) (vec (vector-copy world)))
       (case command
-        ((">") (vector-set! vec (- (vector-length world) 1) (+ point 1)) (brain-eval vec))
-        ((">") (vector-set! vec (- (vector-length world) 1) (- point 1)) (brain-eval vec))
-        (("+") (vector-set! vec point (+ (vector-ref vec point) 1)) (brain-eval vec))
-        (("-") (vector-set! vec point (- (vector-ref vec point) 1)) (brain-eval vec))
-        ((".") (begin (displayln (integer->char (vector-ref vec point))) (brain-eval vec)))
-        ((",") (displayln (integer->char (vector-ref vec point)))
-             (let ((new-v (string-ref (read-line) 0)))
-               (vector-set! vec point (char->integer new-v)) (brain-eval vec)))
+        ((">") (@> vec))
+        (("<") (@< vec))
+        (("+") (@+ vec))
+        (("-") (@- vec))
+        ((".") (|@.| vec))
+        ((",") (|@,| vec))
+        (("[") (|[| vec))
+        (else (brain-eval world))))))
+   
         
-        ))))
+   
   
       ;  ))))
 
